cd5bfe50e7f97ef4056b026b23b06effb987f871
rohitsingh2901 <singhrohitrrr@gmail.com>
2024-02-16 20:05:42 +0530

    Component Type | Server component | Client component

--- src/app/page.js
+++ src/app/page.js
@@ -3,69 +3,22 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <h1>File and folder | Project structure</h1>
-      <h3>package.json</h3>
-      <p>
-      The `package.json` file is a key part of any Node.js project or JavaScript module. It's a JSON file that lives in the root directory of your project and holds important information about the project.
-      </p>
-      <h3>next.config.js</h3>
-      <p>
-      The `next.config.js` is a configuration file used in Next.js, a popular React framework for building web applications. It allows developers to customize various settings and features of their Next.js projects.
-      </p>
-      <h3>jsconfig.json</h3>
-      <p>
-      The `jsconfig.json` file in a Next.js project is a configuration file that helps your text editor understand the structure of your project. It's particularly useful for enabling absolute imports and module path aliases.
-      </p>
-      <h3>README.md</h3>
-      <p>
-      A `README.md` file is a markdown document that serves as the introductory guide to your project. It's often the first thing people see when they visit your repository on platforms like GitHub.
-      </p>
-      <h3>package-lock.json</h3>
-      <p>
-      The `package-lock.json` file is an automatically generated file in Node.js projects that provides a detailed, deterministic record of the dependency tree.
-      </p>
-      <h3>.gitignore</h3>
-      <p>
-      The `.gitignore` file is a text file placed in your git repository that tells git not to track certain files and folders that you don't want being uploaded to your master repository. It's usually used to avoid committing transient files from your working directory that aren't useful to other collaborators, such as compilation products, temporary files IDEs create, etc.
-      </p>
-      <h3>.eslintrc.json</h3>
-      <p>
-      The `.eslintrc.json` file is a configuration file for ESLint, a tool used to identify and report on patterns found in JavaScript code. This file allows you to customize the rules that ESLint will enforce in your project.
-      </p>
-      <h3>.next folder</h3>
-      <p>
-      The `.next` folder is automatically generated when you build your Next.js application. It contains the compiled version of your application and other information that Next.js uses to run your app.
-      </p>
-      <h3>public folder</h3>
-      <p>
-      The `public` folder in a Next.js project is a special directory where you can place static assets that your web application needs.
-      </p>
-      <h3>src folder</h3>
-      <p>
-      The `src` (short for source) directory in a Next.js project is an optional way to organize your project's source code. It typically contains all the components, pages, styles, and other assets needed for your application.
-      </p>
-      <h3>layout.js</h3>
-      <p>
-      In Next.js, `layout.js` is typically used to define a layout component that wraps your application's pages. It allows you to define a consistent structure and appearance for all your pages, making it easy to maintain a consistent user experience.
-      A layout in Next.js is a shared user interface structure used across multiple pages. It serves as the wrapping container for your pages, preserving their state and interactivity during navigation without triggering unnecessary re-renders. Layouts can also be nested to create complex UI structures.
-      </p>
-      <h3>page.js</h3>
-      <p>
-      In Next.js, a `page.js` file represents a page in your web application. Each `.js` file in the `pages` directory corresponds to a route based on its file name.
-      </p>
-      <h3>dependencies vs devDependencies</h3>
-      <p>
-      In a JavaScript or Node.js project, `dependencies` and `devDependencies` in the `package.json` file refer to two types of packages that your project needs, but they serve different purposes:
-
-      1. dependencies: These are the packages or modules that your project needs to run correctly. They are installed when you or someone else installs your package. These could be libraries that your application needs to function effectively.
-
-      2. devDependencies: These are the packages or modules that are only needed during the development process. They are not installed when someone installs your package. These could be testing frameworks, bundlers, or linters.
-      </p>
-
-
-
-
-
+      <h1>Component Type | Server component | Client component</h1>
+      <hr></hr>
+      <h3>Server Component</h3>
+        <li>Render on the Server Side</li>
+        <li>By default, All components are Server components.</li>
+        <li>They are closer to the Backend</li>
+        <li>So BE-related logic and code should write in Server components</li>
+        <img src="ClientServer.png" width={500}></img>
+        <hr></hr>
+      <h3>Client Component</h3>
+        <li>Render on the Client Side</li>
+        <li>Can make client component with `useclient` directive</li>
+        <li>All UI related code and events should be written here</li>
+        <hr></hr>
+      <h3>Composition Pattern</h3>
+      <img src="CompositionPattern.png" width={500}></img>
 
     </main>
   );
