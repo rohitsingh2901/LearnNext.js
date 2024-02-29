dc4b6bb09d80f4ca988087af412b37f9b8df576b
rohitsingh2901 <singhrohitrrr@gmail.com>
2024-02-29 18:04:54 +0530

    Basic Routing | Create new Page

---
+++ src/app/about/page.js
@@ -0,0 +1,8 @@
+const AboutComponent = () =>{
+    return (
+        <div>
+            <h1>About Page</h1>
+        </div>
+    )
+}
+export default AboutComponent;
--- src/app/page.js
+++ src/app/page.js
@@ -3,23 +3,17 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <h1>Component Type | Server component | Client component</h1>
-      <hr></hr>
-      <h3>Server Component</h3>
-        <li>Render on the Server Side</li>
-        <li>By default, All components are Server components.</li>
-        <li>They are closer to the Backend</li>
-        <li>So BE-related logic and code should write in Server components</li>
-        <img src="ClientServer.png" width={500}></img>
-        <hr></hr>
-      <h3>Client Component</h3>
-        <li>Render on the Client Side</li>
-        <li>Can make client component with `useclient` directive</li>
-        <li>All UI related code and events should be written here</li>
-        <hr></hr>
-      <h3>Composition Pattern</h3>
-      <img src="CompositionPattern.png" width={500}></img>
-
+      <h1>Basic Routing | Create new Page </h1>
+      <h3>How to create new page?</h3>
+      <p>
+      Creating a new page in Next.js is straightforward and involves the following steps:<br></br>
+      1. Create a New folder: Inside the root directory of your project, create a new folder whose name is same as your route. The name of this folder will correspond to the route for the new page.<br></br>
+      2. Create a New File: Inside that folder of your project, create a new page.js file.<br></br>
+      3. Export a React Component: In the newly created page.js file, define a React component and export it as the default export. This component will be the content of your new page.<br></br>
+      Example:<br></br>
+      - localhost:3000/login<br></br>
+      - src/login/page.js<br></br>
+      </p>
     </main>
   );
 }
