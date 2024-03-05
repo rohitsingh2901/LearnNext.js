7ee108277f57aa12ccef8f049ab2d2fbf048b3c0
rohitsingh2901 <singhrohitrrr@gmail.com>
2024-03-05 21:30:05 +0530

    Conditional Layout

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,23 +1,28 @@
+"use client";
 import Link from "next/link";
-import "./login.css"
+import "./login.css";
+import { usePathname } from "next/navigation";
 
-export default function Layout({children}){
-    return(
-        <div>
-            
-            <ul className="login-menu">
-                <li>Login Navbar</li>
-                <li>
-                    <Link href="/login">Login Main</Link>
-                </li>
-                <li>
-                    <Link href="/login/studentlogin">Student Login</Link>
-                </li>
-                <li>
-                    <Link href="/login/teacherlogin">Teacher Login </Link>
-                </li>
-            </ul>
-            {children}
-        </div>
-    );
+export default function Layout({ children }) {
+  const pathName = usePathname();
+  return (
+    <div>
+      {pathName !== "/login/teacherlogin" ? (
+        <ul className="login-menu">
+          <li>Login Navbar</li>
+          <li>
+            <Link href="/login">Login Main</Link>
+          </li>
+          <li>
+            <Link href="/login/studentlogin">Student Login</Link>
+          </li>
+          <li>
+            <Link href="/login/teacherlogin">Teacher Login </Link>
+          </li>
+        </ul>
+      ) : <Link href="/">Go to Home Page</Link>}
+
+      {children}
+    </div>
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <h1>Common Layout</h1>
+      <h1>Conditional Layout</h1>
+      <p>
+      In Next.js 13, a conditional layout allows you to dynamically choose which layout to display based on certain conditions. This is particularly useful when you want to display different layouts for different user states or routes, such as whether a user is authenticated or for a particular pathname etc.
+      </p>
     </main>
   );
 }
