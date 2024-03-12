f4b1eba472a61ed91a8553663e0fed789d1c50bf
rohitsingh2901 <singhrohitrrr@gmail.com>
2024-03-12 20:19:25 +0530

    404 page not found

---
+++ src/app/about/[...not-found]/page.js
@@ -0,0 +1,7 @@
+export default function AboutNotFound(){
+    return (
+        <div>
+            <h1>This About Page Not Found</h1>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main>
-      <h1>Dynamic Routing</h1>
+      <h1>404 page not found</h1>
       <p>
-      Catch-all segments in Next.js routing allow you to handle a varying number of URL segments. This is useful when you need to handle a wide range of URL patterns without having to define multiple pages or routes.Here's how it works:
+      In Next.js 13.4, the 404 page is handled by a special `not-found.js` file. This file should be placed in the `app` directory of your project. When a user navigates to a URL that does not match any routes in your application, Next.js will automatically render the `not-found.js` page. If you want route specific 404 page then add [...not-found] folder in that that route directory and and page.js inside it.
       </p>
-      <li>A catch-all segment can be created by adding an ellipsis (`...`) before the parameter name in the filename or folder name, like this: `[...segmentName]`.</li>
-      <li>For example, `pages/shop/[...slug].js` will match `/shop/clothes`, but also `/shop/clothes/tops`, `/shop/clothes/tops/t-shirts`, and so on.</li>
-      <li>The catch-all segment will match any number of segments following the path.</li>
-      <li>Catch-all segments can also be made optional by including the parameter in double square brackets: `[ [...segmentName]]`.</li>
     </main>
   );
 }
